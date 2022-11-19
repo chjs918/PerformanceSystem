7f7b4cc7be594349dffbb338188a1366e319b2c0
seoinee <96633718+seoinee@users.noreply.github.com>
2022-11-19 19:34:51 +0900

    ..

--- PerformSystem/src/main/java/model/Member.java
+++ PerformSystem/src/main/java/model/Member.java
@@ -37,6 +37,55 @@ public class Member {
 		this.myPerformances = myPerformances;
 	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<MyPerformance> getMyPerformances() {
+		return myPerformances;
+	}
+
+	public void setMyPerformances(List<MyPerformance> myPerformances) {
+		this.myPerformances = myPerformances;
+	}
+	 
+
 
 	
 }
---
+++ PerformSystem/src/main/webapp/WEB-INF/myPerformance/dDay.jsp
@@ -0,0 +1,14 @@
+<%@page contentType="text/html; charset=utf-8" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+<title>D-DAY</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script type="text/javascript">
+  window.onload = function() {
+	  var today = new Date();
+	  var 
+  }
+</script>
+</head>
+</html>
