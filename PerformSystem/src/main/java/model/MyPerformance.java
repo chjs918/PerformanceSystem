80c636a2797212526796d92f4c5546e809c9cc64
seoinee <96633718+seoinee@users.noreply.github.com>
2022-11-20 23:09:28 +0900

    UserDAO->MemberDAO변경

--- PerformSystem/src/main/java/model/MyPerformance.java
+++ PerformSystem/src/main/java/model/MyPerformance.java
@@ -1,13 +1,8 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MyPerformance {
 	 private String name;
 	 private String date;
-	 List<>
-	 
 	 
 	public MyPerformance() {
 		super();
@@ -32,23 +27,4 @@ public class MyPerformance {
 		this.date = date;
 	}
 	 
-//	 ArrayList<String> addMyPerformance(String name, String date){
-//		 ArrayList<String> mypf = new ArrayList<String>();
-//		 MyPerformance p = new MyPerformance();
-//		 
-//		 p.setName(name);
-//		 p.setDate(date);
-//		 
-//		 mypf.add(p);
-//	}
-//	 
-//		 MyPerformance deleteMyPerformance(String name, String date){
-//		 MyPerformance<> mp = new MyPerformance(name, date);
-//		 mp.delete(mp)
-//		 return mp;
-//		}
-//		 public MyPerformance getMyPerformance() {
-//		 MyPerformance<List> mp = new MyPerformance<List>(name, date);
-//		 return mp;
-//		}
 }
