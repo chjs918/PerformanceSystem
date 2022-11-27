f756eadd8d0de3c1e356d69961c27d391a6b94e8
yw <yw@DESKTOP-FSU4DRU>
2022-11-27 23:24:37 +0900

    DispatcherServlet 오류 수정

--- PerformSystem/src/main/java/controller/DispatcherServlet.java
+++ PerformSystem/src/main/java/controller/DispatcherServlet.java
@@ -11,19 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 //@WebServlet(name = "MemberSevlet", urlPatterns = "/", loadOnStartup = 1)
-package controller;
-
-import java.io.IOException;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-//@WebServlet(name = "userSevlet", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
